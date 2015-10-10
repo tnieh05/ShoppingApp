ac6a1d42ab4eea0ff87b805dc98c3eb0cde0fcb7
tnieh05 <tnieh05@hotmail.com>
2015-10-09 20:40:30 -0700

    Changes

--- script.js
+++ script.js
@@ -2,16 +2,27 @@ $(document).ready(function(){
 
 	var check = "<i class='fa fa-check-circle'>"
 	var removeItem = "<i class='fa fa-times-circle'>"
+	var invalid = document.getElementById("textbox-enter");
+
+$("input:text").focus(
+	function(){
+		$(this).val('');
+			});
+
+
 
 //This function allows user to press "Enter" to add item to list
 function enterItem() {
 	$("#textbox-enter").keydown(function(enter){
+
 		if(enter.keyCode == 13) {
 			var textInput = $("#textbox-enter").val();
 			var addItems = $("<li>" + check + textInput + removeItem + "</li>");
 			$(".list").append(addItems);
+			$(this).val("");
 			
 		}
+
 	})
 }
 
@@ -42,9 +53,12 @@ $(".main").on("click", ".fa-check-circle", function(){
 
 // Removes a line
 $("ul").on("click", ".fa-times-circle", function(){
-	$(this).parent().closest("li").remove();
+	$(this).parent().closest("li").fadeOut("slow");
 } );
 
+$(document).on("click", "#clear", function(){
+	$(".list").empty();
+});
 	
 });
 
